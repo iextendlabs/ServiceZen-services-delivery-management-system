7439b255520e743ccd00a8ad86a694f6daf6af5c
Ali Hamza <miangdpp@gmail.com>
2023-03-23 17:57:10 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ServiceController;
+use App\Http\Controllers\ServiceStaffController;
+use App\Http\Controllers\ServiceAppointmentController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,6 +23,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::resource('appointments', 'ServiceAppointment');
-Route::resource('services', 'Service');
+
+
+Auth::routes();
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+  
+Route::group(['middleware' => ['auth']], function() {
+    Route::resource('roles', RoleController::class);
+    Route::resource('users', UserController::class);
+    Route::resource('services', ServiceController::class);
+    Route::resource('serviceStaff', ServiceStaffController::class);
+
+});
+Route::resource('appointments', ServiceAppointmentController::class);
 
