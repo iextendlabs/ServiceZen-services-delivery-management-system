9ee1943282f0bcdffe849aa0e289a7045fbd5182
Saqib Ashraf <linktosaqib@gmail.com>
2023-03-27 20:05:46 +0500

    Account Drop Down

--- resources/views/site/layout/app.blade.php
+++ resources/views/site/layout/app.blade.php
@@ -36,10 +36,16 @@
                   <a class="nav-link" href="#">About</a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="#">Login</a>
-                </li>
-                <li class="nav-item">
                   <a class="nav-link" href="#">Contact</a>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Account
+                  </a>
+                  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Login</a>
+                    <a class="dropdown-item" href="#">Register</a>
+                  </div>
                 </li>
               </ul>
             </div>
