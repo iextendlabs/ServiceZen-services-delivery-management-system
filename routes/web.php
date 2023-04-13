771baecf452a872eca7c730a4ee95046a151df83
Ali Hamza <miangdpp@gmail.com>
2023-04-13 22:30:50 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -58,6 +58,6 @@ Route::get('customer-logout', [CustomerAuthController::class, 'logout']);
 Route::get('booking/{id}', [ServiceAppointmentController::class, 'create']);
 Route::resource('booking', ServiceAppointmentController::class);
 // Order
-Route::get('checkout/{id}', 'App\Http\Controllers\site\OrderController@checkout');
-Route::get('CartCheckout', 'App\Http\Controllers\site\OrderController@CartCheckout');
-Route::resource('order', 'App\Http\Controllers\site\OrderController');
+Route::get('checkout/{id}', 'App\Http\Controllers\Site\OrderController@checkout');
+Route::get('CartCheckout', 'App\Http\Controllers\Site\OrderController@CartCheckout');
+Route::resource('order', 'App\Http\Controllers\Site\OrderController');
