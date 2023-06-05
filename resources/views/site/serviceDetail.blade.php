028f6bbfd540162b840acc31e40245851f9d6df1
Ali Hamza <miangdpp@gmail.com>
2023-06-05 21:13:51 +0500

    media share

--- resources/views/site/serviceDetail.blade.php
+++ resources/views/site/serviceDetail.blade.php
@@ -1,47 +1,58 @@
 @extends('site.layout.app')
 <base href="/public">
 <style>
-    .box-shadow {
-        background: none !important;
-    }
+  .box-shadow {
+    background: none !important;
+  }
 </style>
 @section('content')
 <section class="jumbotron text-center">
   <div class="container">
-      <h1 class="jumbotron-heading">Best In the Town Saloon Services</h1>
-      <p class="lead text-muted">Get Your Desired Saloon Beauty service at Your Door, easy to schedule and just few clicks away.</p>
+    <h1 class="jumbotron-heading">Best In the Town Saloon Services</h1>
+    <p class="lead text-muted">Get Your Desired Saloon Beauty service at Your Door, easy to schedule and just few clicks away.</p>
   </div>
 </section>
 <div class="text-center">
   @if(Session::has('error'))
-    <span class="alert alert-danger" role="alert">
-      <strong>{{ Session::get('error') }}</strong>
-    </span>
-    @endif
-    @if(Session::has('success'))
-    <span class="alert alert-success" role="alert">
-      <strong>{{ Session::get('success') }}</strong>
-    </span>
-    @endif
+  <span class="alert alert-danger" role="alert">
+    <strong>{{ Session::get('error') }}</strong>
+  </span>
+  @endif
+  @if(Session::has('success'))
+  <span class="alert alert-success" role="alert">
+    <strong>{{ Session::get('success') }}</strong>
+  </span>
+  @endif
 </div>
 <div class="album py-5 bg-light">
   <div class="container">
+    <!-- AddToAny BEGIN -->
+    <div class="a2a_kit a2a_kit_size_32 a2a_default_style" style="margin-bottom: 20px;">
+      <a class="a2a_dd" href="https://www.addtoany.com/share"></a>
+      <a class="a2a_button_facebook"></a>
+      <a class="a2a_button_twitter"></a>
+      <a class="a2a_button_whatsapp"></a>
+      <a class="a2a_button_telegram"></a>
+    </div>
+    <script async src="https://static.addtoany.com/menu/page.js"></script>
+    <!-- AddToAny END -->
     <div class="row">
-        <div class="col-md-8">
-          <img class="card-img-top" src="./service-images/{{ $service->image }}" alt="Card image cap">
-        </div>
-        <div class="col-md-4 box-shadow">
-          <h4 class="card-text"><b>{{ $service->name }}</b></h4>
-          <div class="card-body">
-            <p class="card-text">{!! $service->description !!}</p>
-            <p class="card-text"><b>Duration:</b>{{ $service->duration }}</p>
-            <p class="card-text"><b>Price:</b>${{ $service->price }}</p>
-            <div class="btn-group">
-                <a href="/addToCart/{{ $service->id }}"><button type="button" class="btn btn-sm btn-outline-secondary">Add to Cart</button></a>
-            </div>
+      <div class="col-md-8">
+        <img class="card-img-top" src="./service-images/{{ $service->image }}" alt="Card image cap">
+      </div>
+      <div class="col-md-4 box-shadow">
+        <h4 class="card-text"><b>{{ $service->name }}</b></h4>
+        <div class="card-body">
+          <p class="card-text">{!! $service->description !!}</p>
+          <p class="card-text"><b>Duration:</b>{{ $service->duration }}</p>
+          <p class="card-text"><b>Price:</b>${{ $service->price }}</p>
+          <div class="btn-group">
+            <a href="/addToCart/{{ $service->id }}"><button type="button" class="btn btn-sm btn-outline-secondary">Add to Cart</button></a>
           </div>
         </div>
-    </div><hr>
+      </div>
+    </div>
+    <hr>
     @if(count($service->package))
     <h2>Package</h2><br>
     <div class="row">
