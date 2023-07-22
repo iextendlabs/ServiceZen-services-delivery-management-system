bbf3268fd379c377d8fafb5130aaf9a3efc2d194
Saqib Ashraf <linktosaqib@gmail.com>
2023-07-22 14:27:56 +0500

    - comment fix

--- resources/views/cashCollections/index.blade.php
+++ resources/views/cashCollections/index.blade.php
@@ -36,7 +36,7 @@
                     <td>@currency( $cash_collection->order->total_amount )</td>
                     <td>{{ $cash_collection->description }}</td>
                     
-                    <td> $cash_collection->order->comment</td>
+                    <td> {{$cash_collection->order->comment}}</td>
 
                     <td>{{ $cash_collection->status }}</td>
                     <td>
