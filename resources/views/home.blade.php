87c967175c079e9361ae6250cd94287262d26f8d
Ali Hamza <miangdpp@gmail.com>
2023-07-24 19:09:21 +0500

    dashboard order list update for supervisor

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -54,22 +54,22 @@
             <div class="float-end">
                 <!-- All Orders -->
                 <a class="btn btn-secondary ml-2 float-end" href="/orders" style="margin-right: 10px;">
-                <i class="fas fa-list"></i> All Orders
+                    <i class="fas fa-list"></i> All Orders
                 </a>
 
                 <!-- Pending Order -->
                 <a class="btn btn-primary float-end" href="/orders?status=Pending" style="margin-right: 10px;">
-                <i class="fas fa-clock"></i> Pending Order
+                    <i class="fas fa-clock"></i> Pending Order
                 </a>
 
                 <!-- Complete Order -->
                 <a class="btn btn-success float-end" href="/orders?status=Complete" style="margin-right: 10px;">
-                <i class="fas fa-check"></i> Complete Order
+                    <i class="fas fa-check"></i> Complete Order
                 </a>
 
                 <!-- Canceled Order -->
                 <a class="btn btn-danger float-end" href="/orders?status=Canceled" style="margin-right: 10px;">
-                <i class="fas fa-times"></i> Canceled Order
+                    <i class="fas fa-times"></i> Canceled Order
                 </a>
 
             </div>
@@ -79,63 +79,74 @@
         <div class="col-md-12">
             <div class="card">
                 <div class="card-body">
-                <table class="table table-bordered table-responsive">
-                <tr>
-                    <th>No</th>
-                    <th>Order Id</th>
-                    <th>Customer</th>
-                    <th>Staff</th>
-                    <th>Data \ Time Slot</th>
-                    <th>Total Amount</th>
-                    <th>Payment Method</th>
-                    <th>Status</th>
-                    <th>Comment</th>
-                    <th>Date Added</th>
-                    <th>Action</th>
-                </tr>
-                @if(count($orders))
-                @foreach ($orders as $order)
-                <tr>
-                    <td>{{ ++$i }}</td>
-                    <td>#{{ $order->id }}</td>
+                    <table class="table table-bordered table-responsive">
+                        <tr>
+                            <th>Order #</th>
+                            <th>Staff</th>
+                            <th>Data \ Time Slot</th>
+                            @if(auth()->user()->getRoleNames() == '["Supervisor"]')
+                            <th>Landmark</th>
+                            <th>Area</th>
+                            <th>City</th>
+                            <th>Building name</th>
+                            @else
+                            <th>Customer</th>
+                            <th>Total Amount</th>
+                            <th>Payment Method</th>
+                            <th>Comment</th>
+                            @endif
+                            <th>Status</th>
+                            <th>Date Added</th>
+                            <th style="min-width:160px">Action</th>
+                        </tr>
+                        @if(count($orders))
+                        @foreach ($orders as $order)
+                        <tr>
+                            <th>#{{ $order->id }}</th>
+                            <td>{{ $order->staff_name }}</td>
+                            <td>{{ $order->date }} \ {{ $order->time_slot_value }}</td>
+                            @if(auth()->user()->getRoleNames() == '["Supervisor"]')
+                            <td>{{ $order->landmark }}</td>
+                            <td>{{ $order->area }}</td>
+                            <td>{{ $order->city }}</td>
+                            <td>{{ $order->buildingName }}</td>
+                            @else
+                            <td>{{ $order->customer_name }}</td>
+                            <td>@currency($order->total_amount)</td>
+                            <td>{{ $order->payment_method }}</td>
+                            <td>{{ $order->order_comment }}</td>
+                            @endif
+                            <td>{{ $order->status }}</td>
+                            <td>{{ $order->created_at }}</td>
+                            <td>
+                                <form action="{{ route('orders.destroy',$order->id) }}" method="POST">
+                                    <a class="btn btn-info" href="{{ route('orders.show',$order->id) }}">
+                                        <i class="fas fa-eye"></i>
+                                    </a>
 
-                    <td>{{ $order->customer_name }}</td>
-                    <td>{{ $order->staff_name }}</td>
-                    <td>{{ $order->date }} \ {{ $order->time_slot_value }}</td>
-                    <td>@currency($order->total_amount)</td>
-                    <td>{{ $order->payment_method }}</td>
-                    <td>{{ $order->status }}</td>
-                    <td>{{ $order->order_comment }}</td>
-                    <td>{{ $order->created_at }}</td>
-                    <td>
-                        <form action="{{ route('orders.destroy',$order->id) }}" method="POST">
-                            <a class="btn btn-info" href="{{ route('orders.show',$order->id) }}">
-                            <i class="fas fa-eye"></i> 
-                            </a>
+                                    @can('order-edit')
+                                    <a class="btn btn-primary" href="{{ route('orders.edit',$order->id) }}">
+                                        <i class="fas fa-edit"></i>
+                                    </a>
+                                    @endcan
+                                    @csrf
+                                    @method('DELETE')
+                                    @can('order-delete')
+                                    <button type="submit" class="btn btn-danger">
+                                        <i class="fas fa-trash"></i>
+                                    </button>
 
-                            @can('order-edit')
-                            <a class="btn btn-primary" href="{{ route('orders.edit',$order->id) }}">
-                            <i class="fas fa-edit"></i> 
-                            </a>
-                            @endcan
-                            @csrf
-                            @method('DELETE')
-                            @can('order-delete')
-                            <button type="submit" class="btn btn-danger">
-                                <i class="fas fa-trash"></i> 
-                            </button>
-
-                            @endcan
-                        </form>
-                    </td>
-                </tr>
-                @endforeach
-                @else
-                <tr>
-                    <td colspan="11" class="text-center"> There is no Order</td>
-                </tr>
-                @endif
-            </table>
+                                    @endcan
+                                </form>
+                            </td>
+                        </tr>
+                        @endforeach
+                        @else
+                        <tr>
+                            <td colspan="11" class="text-center"> There is no Order</td>
+                        </tr>
+                        @endif
+                    </table>
                 </div>
             </div>
         </div>
