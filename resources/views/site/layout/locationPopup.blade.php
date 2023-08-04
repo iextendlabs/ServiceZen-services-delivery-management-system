3f5c605f1c691e203c2b1484ae51e16f5c2bed16
Saqib Ashraf <linktosaqib@gmail.com>
2023-08-05 00:40:08 +0500

    - popup ui update

--- resources/views/site/layout/locationPopup.blade.php
+++ resources/views/site/layout/locationPopup.blade.php
@@ -48,8 +48,7 @@
 
       <!-- Popup Footer -->
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save Changes</button>
+        <button type="button" class="btn btn-block btn-primary">Save Location</button>
       </div>
 
     </div>
