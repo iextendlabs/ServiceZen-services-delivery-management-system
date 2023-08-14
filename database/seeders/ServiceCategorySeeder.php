e92cdec6f9506da159bed50f249c8de815c80533
Ali Hamza <miangdpp@gmail.com>
2023-08-14 20:32:58 +0500

    config change

--- database/seeders/ServiceCategorySeeder.php
+++ database/seeders/ServiceCategorySeeder.php
@@ -19,32 +19,26 @@ class ServiceCategorySeeder extends Seeder
             [
                 'title' => 'Hair Services',
                 'description' => 'Stylish haircuts, coloring, and treatments to give you a fresh look.',
-                'parent_id'=>null
             ],
             [
                 'title' => 'Nail Services',
                 'description' => 'Manicures, pedicures, and nail treatments to keep your nails looking beautiful.',
-                'parent_id'=>null
             ],
             [
                 'title' => 'Waxing Services',
                 'description' => 'Professional waxing services to remove unwanted hair.',
-                'parent_id'=>null
             ],
             [
                 'title' => 'Makeup Services',
                 'description' => 'Professional makeup application for special occasions or everyday wear.',
-                'parent_id'=>null
             ],
             [
                 'title' => 'Skin Care Services',
                 'description' => 'Facials, peels, and other skin care treatments to help you achieve healthy, glowing skin.',
-                'parent_id'=>null
             ],
             [
                 'title' => 'Eyelash Services',
                 'description' => 'Lash extensions, lifts, and other treatments to enhance your natural lashes.',
-                'parent_id'=>null
             ]
         ];
 
