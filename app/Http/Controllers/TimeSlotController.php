fee936fd05affcb4a21fce9464694bcb89a305e2
Ali Hamza <miangdpp@gmail.com>
2023-08-16 22:19:03 +0500

    time slots order by time

--- app/Http/Controllers/TimeSlotController.php
+++ app/Http/Controllers/TimeSlotController.php
@@ -34,7 +34,7 @@ class TimeSlotController extends Controller
      */
     public function index()
     {
-        $time_slots = TimeSlot::latest()->orderBy('time_start')->paginate(config('app.paginate'));
+        $time_slots = TimeSlot::orderBy('time_start')->paginate(config('app.paginate'));
         return view('timeSlots.index', compact('time_slots'))
             ->with('i', (request()->input('page', 1) - 1) * config('app.paginate'));
     }
