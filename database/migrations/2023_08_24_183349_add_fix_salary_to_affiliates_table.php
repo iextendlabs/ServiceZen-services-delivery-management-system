6e379352dced03c3a448dc9274aa4a7c610f644f
Ali Hamza <miangdpp@gmail.com>
2023-08-24 20:31:12 +0500

    Create 2023_08_24_183349_add_fix_salary_to_affiliates_table.php

---
+++ database/migrations/2023_08_24_183349_add_fix_salary_to_affiliates_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('affiliates', function (Blueprint $table) {
+            $table->decimal('fix_salary', 8, 2)->after('commission')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('affiliates', function (Blueprint $table) {
+            $table->dropColumn('fix_salary');
+        });
+    }
+};
