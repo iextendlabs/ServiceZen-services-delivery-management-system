fffd2b8c4b9f5d6edf790501dc80f60e6d11045c
Ali Hamza <miangdpp@gmail.com>
2023-09-06 20:50:18 +0500

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -225,11 +225,10 @@
     </footer>
 
 
-    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.10.2/fullcalendar.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script> -->
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
     <script>
         $(document).ready(function() {
