48f39df0b233fb7e3240c1876cd036b5a632fa08
Ali Hamza <miangdpp@gmail.com>
2023-10-18 15:43:09 +0500

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -18,7 +18,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
         $schedule->command('orders:send-email')
-             ->dailyAt('7:00');
+             ->dailyAt('15:45');
     }
 
     /**
