8e1f3a11fd29c3ef5731d2f9e69b3c0e682664c8
Saqib Ashraf <linktosaqib@gmail.com>
2023-11-07 01:57:56 +0500

    - log bug fix

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -73,5 +73,6 @@ class Kernel extends HttpKernel
         'permission' => \Spatie\Permission\Middlewares\PermissionMiddleware::class,
         'role_or_permission' => \Spatie\Permission\Middlewares\RoleOrPermissionMiddleware::class,
         'no-cache' => \App\Http\Middleware\NoCacheMiddleware::class,
+        'log.api' => \App\Http\Middleware\LogApiRequestsAndResponses::class
     ];
 }
