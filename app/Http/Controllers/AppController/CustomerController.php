b7beca325963c09bc26f74c9d7d633c2e0506758
Ali Hamza <miangdpp@gmail.com>
2023-11-28 17:09:23 +0500

    get staff for customer app

--- app/Http/Controllers/AppController/CustomerController.php
+++ app/Http/Controllers/AppController/CustomerController.php
@@ -5,10 +5,16 @@ namespace App\Http\Controllers\AppController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
+use App\Models\Holiday;
+use App\Models\LongHoliday;
 use App\Models\Service;
 use App\Models\ServiceCategory;
 use App\Models\Setting;
+use App\Models\Staff;
+use App\Models\StaffGeneralHoliday;
+use App\Models\StaffHoliday;
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
@@ -76,10 +82,10 @@ class CustomerController extends Controller
 
     public function index()
     {
-        
+
         $slider_images = Setting::where('key', 'Slider Image')->value('value');
 
-        $images = explode(",",$slider_images);
+        $images = explode(",", $slider_images);
 
         $categories = ServiceCategory::get();
         $services = Service::take(10)->get();
@@ -89,5 +95,40 @@ class CustomerController extends Controller
             'categories' => $categories,
             'services' => $services,
         ], 200);
+    }
+
+    public function availableStaff(Request $request)
+    {
+        $carbonDate = Carbon::createFromFormat('Y-m-d', $request->date);
+
+        $staff_ids = StaffHoliday::where('date', $request->date)->pluck('staff_id')->toArray();
+
+        $dayName = $carbonDate->formatLocalized('%A');
+        $generalHolidays = config('app.general_holiday');
+        if (in_array($dayName, $generalHolidays)) {
+            $holiday[] = $request->date;
+        } else {
+            $holiday = Holiday::where('date', $request->date)->pluck('date')->toArray();
+        }
+        $generalHolidayStaffIds = StaffGeneralHoliday::where('day', $dayName)->pluck('staff_id')->toArray();
+
+        $longHolidaysStaffId = LongHoliday::where('date_start', '<=', $request->date)
+            ->where('date_end', '>=', $request->date)
+            ->pluck('staff_id')->toArray();
+        $on_holiday_staff_ids = array_unique([...$staff_ids, ...$generalHolidayStaffIds, ...$longHolidaysStaffId]);
+
+        if ($holiday) {
+            return response()->json([
+                'msg' => "Whoops! Holiday on selected date. Please choose another date by Click on Data.",
+            ], 201);
+        } else {
+            $availableStaff = User::with('staff')->whereHas('staff', function ($query) use ($on_holiday_staff_ids) {
+                $query->where('status', 1)->whereNotIn('user_id', $on_holiday_staff_ids);
+            })->get();
+
+            return response()->json([
+                'availableStaff' => $availableStaff
+            ], 200);
+        }
     }
 }
