2a501a68813efc7b6fd3769421b54b5d1182f525
Ali Hamza <86736070+Hamza00003@users.noreply.github.com>
2024-04-19 18:03:07 +0500

    Update bookingStep.blade.php

--- resources/views/site/checkOut/bookingStep.blade.php
+++ resources/views/site/checkOut/bookingStep.blade.php
@@ -104,7 +104,7 @@
                         <div class="form-group">
                             <span style="color: red;">*</span><strong>Area:</strong>
     
-                            <select disabled required class="form-control" name="area">
+                            <select readonly required class="form-control" name="area">
                                 <option value="">-- Select Zone -- </option>
                                     <!-- Loop through the $zones array to generate options -->
                                 @foreach ($zones as $zone)
