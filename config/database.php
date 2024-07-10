50ef34bd44a7092c1a2e20e42bda10ae1554b860
Ali Hamza <86736070+Hamza00003@users.noreply.github.com>
2024-07-10 13:06:38 +0500

    Update database.php

--- config/database.php
+++ config/database.php
@@ -44,7 +44,7 @@ return [
         ],
 
         'mysql' => [
-            // 'timezone' => 'Asia/Dubai',
+            'timezone' => 'Asia/Dubai',
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
