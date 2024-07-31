ea0717c6dd728d0b2ec32cb8d7d8639a02ec1e99
Ali Hamza <86736070+Hamza00003@users.noreply.github.com>
2024-07-31 18:19:32 +0500

    Update StripePaymentController.php

--- app/Http/Controllers/AppController/StripePaymentController.php
+++ app/Http/Controllers/AppController/StripePaymentController.php
@@ -8,17 +8,19 @@ use App\Models\StaffZone;
 use Illuminate\Http\Request;
 use Stripe\Stripe;
 use Stripe\PaymentIntent;
+use Stripe\Customer;
 
 class StripePaymentController extends Controller
 {
     public function createPaymentIntent(Request $request)
     {
-        // $amount = $request->input('amount');
-        // $order_ids = $request->input('order_ids');
+        $amount = $request->amount;
+        $order_ids = $request->order_ids;
+        $email = $request->email;
+        $name = $request->name;
 
         // $orders = Order::whereIn('id', explode(',', $order_ids))->get();
-        // $order_total = 0;
-        // $order_total += $orders->sum(function ($order) {
+        // $order_total = $orders->sum(function ($order) {
         //     return $order->total_amount;
         // });
 
@@ -32,12 +34,24 @@ class StripePaymentController extends Controller
 
         Stripe::setApiKey(env('STRIPE_SECRET'));
 
-        $paymentIntent = PaymentIntent::create([
-            'amount' => $request->input('amount'),
-            'currency' => "AED",
-            "description" => "New Order Payment from Lipslay web store. Order ids are ".$request->order_ids
-        ]);
+        try {
+            // Create a customer
+            $customer = Customer::create([
+                'email' => $email,
+                'name' => $name,
+            ]);
 
-        return response()->json(['client_secret' => $paymentIntent->client_secret]);
+            // Create a payment intent with the customer ID
+            $paymentIntent = PaymentIntent::create([
+                'amount' => $amount * 100,
+                'currency' => "AED",
+                'description' => "New Order Payment from Lipslay web store. Order ids are ".$order_ids,
+                'customer' => $customer->id,
+            ]);
+
+            return response()->json(['client_secret' => $paymentIntent->client_secret], 200);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
     }
 }
