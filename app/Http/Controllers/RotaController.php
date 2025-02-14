87d822ce61ef098c2c67542c1c79012cae1f018c
Ali Hamza <86736070+Hamza00003@users.noreply.github.com>
2025-02-14 20:02:16 +0500

    rota update

--- app/Http/Controllers/RotaController.php
+++ app/Http/Controllers/RotaController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
 use App\Models\Order;
+use Illuminate\Support\Facades\Auth;
 
 class RotaController extends Controller
 {
@@ -15,7 +16,43 @@ class RotaController extends Controller
             $currentDate = Carbon::now()->toDateString();
         }
 
-        $orders = Order::where('date', $currentDate)->get()->toArray();
+        $currentUser = Auth::user();
+
+        $query = Order::where('date', $currentDate);
+        $userRole = $currentUser->getRoleNames()->first(); // Assuming you have a variable that holds the user's role, e.g., $userRole = $currentUser->getRole();
+
+        switch ($userRole) {
+            case 'Manager':
+                $staffIds = $currentUser->getManagerStaffIds();
+                $query = $query->whereIn('service_staff_id', $staffIds)->orderBy('date', 'ASC')->orderBy('time_start');
+                break;
+
+            case 'Supervisor':
+                $staffIds = $currentUser->getSupervisorStaffIds();
+                $query = $query->whereIn('service_staff_id', $staffIds)
+                    ->where(function ($query) {
+                        $query->whereDoesntHave('cashCollection');
+                    })
+                    ->orderBy('date', 'ASC')->orderBy('time_start');
+                break;
+
+            case 'Staff':
+                $query = $query->where('service_staff_id', Auth::id())
+                ->orderBy('date', 'ASC')
+                ->orderBy('time_start')
+                ->where('date', '=', $currentDate)
+                ->where(function ($query) {
+                    $query->whereIn('status', ['Complete', 'Confirm', 'Accepted'])
+                        ->whereDoesntHave('cashCollection');
+                });
+                
+                break;
+
+            default:
+                $query;
+                break;
+        }
+        $orders = $query->get();
         return view('graph.index', [
             'orders' => $orders,
             'date' => $currentDate
