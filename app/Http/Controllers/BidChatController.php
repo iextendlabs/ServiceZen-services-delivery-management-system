7cec411735b86177a5d8b6b705749ea000aa4102
Ali Hamza <miangdpp@gmail.com>
2025-03-25 01:17:24 +0500

    location in bid chat for staff app

--- app/Http/Controllers/BidChatController.php
+++ app/Http/Controllers/BidChatController.php
@@ -41,6 +41,9 @@ class BidChatController extends Controller
     
             $messageData['message'] = $filename;
             $messageData['file'] = 1;
+        } elseif($request->hasFile('location')) {
+            $messageData['location'] = 1;
+            $messageData['message'] = $request->message;
         } else {
             $messageData['message'] = $request->message;
         }
