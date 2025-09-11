00b7505f7d0718b9456287a139e198b8bbf2b874
Ali Hamza <86736070+Hamza00003@users.noreply.github.com>
2025-09-11 18:51:08 +0500

    Update CustomerController.php

--- app/Http/Controllers/AppController/CustomerController.php
+++ app/Http/Controllers/AppController/CustomerController.php
@@ -57,11 +57,11 @@ class CustomerController extends Controller
         if (!$user) {
             return response()->json(['error' => 'User not found'], 404);
         }
-        $user->balance = number_format((float)$request->balance, 2, '.', '');
+        $user->balance = (int)$request->balance;
         $user->save();
         return response()->json(['balance' => $user->balance], 200);
     }
-    
+
     public function __construct()
     {
         $this->middleware('log.api');
